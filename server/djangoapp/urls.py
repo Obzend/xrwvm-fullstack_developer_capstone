97d29e51b85789ef875062a80d090823ffd2730f
Obzend <obz3nd@gmail.com>
2024-09-03 08:16:28 -0400

    temp6

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -10,6 +10,12 @@ urlpatterns = [
 
     # path for login
     # path(route='login', view=views.login_user, name='login'),
+    # path for registration
+    path(route='register', view=views.registration, name='register'),
+    # path for login
+    path(route='login', view=views.login_user, name='login'),
+    # path for logout
+    path(route='logout', view=views.logout_request, name='logout'),
 
     # path for dealer reviews view
 
@@ -20,5 +26,5 @@ urlpatterns = [
     path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
     path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
     path(route='add_review', view=views.add_review, name='add_review'),
-
+    path(route='get_dealers', view=views.get_dealerships, name='get_dealers')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
